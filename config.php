b1a29880bed3b3db86a5b23cfef060da27ff351c
AO™ <anthony@osawere.com>
2023-07-10 10:43:57 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+//*** Config - Configuration » Tydi™ Framework © 2023 ∞ AO™ • @iamodao • www.osawere.com ∞ Apache License ***//
+const MD = [
+	'LIBRY' => RD . 'libry' . DS,
+	'ORIG' => RD . 'orig' . DS
+];
+const MACHINE = 'LOCAL';
+const ENV = 'DEVELOPMENT';
+const SYS = 'ONLINE';
+const HOST = [
+	'MAIN' => [
+		'NAME' => 'tydi.co'
+	],
+	'JORE' => [
+		'NAME' => 'jore.co'
+	]
+];
